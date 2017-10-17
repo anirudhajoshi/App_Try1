3b93df6de04410da8db5c75c150f2d979e5e6b10
Anirudha Joshi <anirudhajoshi@yahoo.com>
2017-10-17 16:44:08 -0400

    First screen UI and function hooks

--- app/src/main/java/com/example/aniru/publibapp/MainActivity.java
+++ app/src/main/java/com/example/aniru/publibapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.aniru.publibapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void OnOk(View v) {
+        Toast.makeText(this, "OnOk", Toast.LENGTH_SHORT).show();
+    }
+
+    public void OnCamera(View v) {
+        Toast.makeText(this, "OnCamera", Toast.LENGTH_SHORT).show();
+    }
+
+    public void OnArchive(View v) {
+        Toast.makeText(this, "OnArchive", Toast.LENGTH_SHORT).show();
+    }
+
+    public void OnFavorites(View v) {
+        Toast.makeText(this, "OnFavs", Toast.LENGTH_SHORT).show();
+    }
+
 }
